6da53e99f77221d3a61d821572864443e8d73314
Marten Seemann <martenseemann@gmail.com>
2025-12-28 16:41:22 +0800

    http3: add a new API to be used by WebTransport

--- http3/transport.go
+++ http3/transport.go
@@ -421,7 +421,7 @@ func (t *Transport) removeClient(hostname string) {
 // Obtaining a ClientConn is only needed for more advanced use cases, such as
 // using Extended CONNECT for WebTransport or the various MASQUE protocols.
 func (t *Transport) NewClientConn(conn *quic.Conn) *ClientConn {
-	return newClientConn(
+	c := newClientConn(
 		conn,
 		t.EnableDatagrams,
 		t.AdditionalSettings,
@@ -429,6 +429,33 @@ func (t *Transport) NewClientConn(conn *quic.Conn) *ClientConn {
 		t.DisableCompression,
 		t.Logger,
 	)
+	go func() {
+		for {
+			str, err := conn.AcceptUniStream(context.Background())
+			if err != nil {
+				return
+			}
+			go c.handleUnidirectionalStream(str)
+		}
+	}()
+	return c
+}
+
+// NewRawClientConn creates a new low-level HTTP/3 client connection on top of a QUIC connection.
+// Unlike NewClientConn, the returned RawClientConn allows the application to take control
+// of the stream accept loops, by calling HandleUnidirectionalStream for incoming unidirectional
+// streams and HandleBidirectionalStream for incoming bidirectional streams.
+func (t *Transport) NewRawClientConn(conn *quic.Conn) *RawClientConn {
+	return &RawClientConn{
+		ClientConn: newClientConn(
+			conn,
+			t.EnableDatagrams,
+			t.AdditionalSettings,
+			t.MaxResponseHeaderBytes,
+			t.DisableCompression,
+			t.Logger,
+		),
+	}
 }
 
 // Close closes the QUIC connections that this Transport has used.
